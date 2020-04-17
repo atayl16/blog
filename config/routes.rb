965cf8c860e865cf8fead31b8128a0738498f361
Alisha Taylor <atayl16@yahoo.com>
2020-04-17 02:11:42 +0100

    Added session controller and support

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  root 'welcome#home'
+  get 'about', to: 'welcome#about'
+
   resources :articles
-  root 'pages#home'
-  get '/about' => 'pages#about'
-  get 'signup', to: 'users#new'
+
+  get 'signup', to: "users#new"
   resources :users, except: [:new]
+  get 'login', to: "sessions#new"
+  post 'login', to: "sessions#create"
+  delete 'logout', to: "sessions#destroy"
 end
